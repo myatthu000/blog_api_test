eddf153a7213b1af782e964a9dc0590cdf31ac7c
myatthu000 <september.2019thu@gmail.com>
2023-10-27 14:33:59 +0630

    notification

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use App\Http\Resources\UserResource;
+use App\Models\Post;
 use App\Models\User;
+use App\Notifications\NewPostNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,15 +26,45 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return new UserResource($request->user());
 });
 
 
-Route::apiResource('category', CategoryController::class);
-Route::apiResource('post', PostController::class);
+//Route::middleware(['auth:sanctum'])->get('/api/user',function (Request $request){
+//    return new $request->user();
+//});
 
 
+Route::middleware(['auth:sanctum'])->post('/notification-send',function (Request $request){
 
-Route::get('user',function (){
-    return UserResource::collection(User::all());
+    $subscribers = Post::query()->find(7);
+
+    return $subscribers;
+//    foreach ($subscribers as $subscriber) {
+//        Log::debug($subscriber);
+////        auth()->user()->notify(new NewPostNotification($subscriber));
+//    }
+//
+//    dd('done');
+
 });
+
+Route::get('onePost',[PostController::class,'onePost']);
+
+Route::apiResource('category', CategoryController::class)->middleware(['auth:sanctum']);
+
+Route::apiResource('post', PostController::class)
+    ->middleware(['auth:sanctum'])
+;
+
+Route::post('/sanctum/register', [AuthController::class,'register']);
+
+Route::post('/sanctum/login', [AuthController::class,'login']);
+
+Route::post('/sanctum/changeInfo', [AuthController::class,'changePassword'])->middleware(['auth:sanctum']);
+
+Route::delete('/sanctum/logout', [AuthController::class,'logout']);
+
+Route::delete('/sanctum/logout/all', [AuthController::class,'logoutAll'])->middleware(['auth:sanctum']);
+
+Route::post('/sanctum/tokens', [AuthController::class,'tokens'])->middleware(['auth:sanctum']);

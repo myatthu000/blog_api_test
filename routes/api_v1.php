bc70875011d68da0230c90398dc608f6c481c5c9
myatthu000 <september.2019thu@gmail.com>
2023-11-01 15:39:27 +0630

    route-update

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,17 +2,16 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SubscribedController;
+use App\Http\Controllers\UserController;
 use App\Http\Resources\UserResource;
 use App\Models\Post;
 use App\Models\User;
-use App\Notifications\NewPostNotification;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,46 +24,77 @@ use Illuminate\Validation\ValidationException;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return new UserResource($request->user());
-});
-
-
-//Route::middleware(['auth:sanctum'])->get('/api/user',function (Request $request){
-//    return new $request->user();
-//});
-
-
-Route::middleware(['auth:sanctum'])->post('/notification-send',function (Request $request){
-
-    $subscribers = Post::query()->find(7);
-
-    return $subscribers;
-//    foreach ($subscribers as $subscriber) {
-//        Log::debug($subscriber);
-////        auth()->user()->notify(new NewPostNotification($subscriber));
-//    }
-//
-//    dd('done');
-
+Route::get('/all-user',function (Request $request){
+    $users = User::query()->where('role','like','user')->get();
+    return $users;
 });
 
 Route::get('onePost',[PostController::class,'onePost']);
 
-Route::apiResource('category', CategoryController::class)->middleware(['auth:sanctum']);
+Route::group([
+    'prefix' => 'sanctum',
+],function (){
+    Route::post('/register', [AuthController::class,'register'])->name('register');
+    Route::post('/login', [AuthController::class,'login'])->name('login');
+});
 
-Route::apiResource('post', PostController::class)
-    ->middleware(['auth:sanctum'])
-;
+Route::group([
+    'middleware' => ['auth:sanctum'],
+],function (){
 
-Route::post('/sanctum/register', [AuthController::class,'register']);
+    Route::apiResource('category', CategoryController::class)->middleware(['auth:sanctum']);
+    Route::apiResource('post', PostController::class)->middleware(['auth:sanctum']);
 
-Route::post('/sanctum/login', [AuthController::class,'login']);
+    Route::get('/user', function (Request $request) {
+        return new UserResource($request->user());
+    });
+    Route::get('/user/{id}', function (Request $request) {
+        return new UserResource(User::findOrFail(\request('id')));
+    });
 
-Route::post('/sanctum/changeInfo', [AuthController::class,'changePassword'])->middleware(['auth:sanctum']);
+    Route::prefix('sanctum')->group(function (){
+        Route::post('/changeInfo', [AuthController::class,'changePassword'])->name('auth.changeInfo');
+        Route::delete('/logout', [AuthController::class,'logout'])->name('auth.logout');
+        Route::delete('/logout/all', [AuthController::class,'logoutAll'])->name('auth.logoutAll');
+        Route::post('/tokens', [AuthController::class,'tokens'])->name('auth.tokens'); //get user's token
+    });
 
-Route::delete('/sanctum/logout', [AuthController::class,'logout']);
+    Route::prefix('notifications')->group(function (){
+        Route::get('/',[NotificationController::class,'notifications'])->name('noti.all');
+        Route::get('/choose',[NotificationController::class,'optionTypes'])->name('noti.types');
+        Route::get('/read/{id}',[NotificationController::class,'markAsRead'])->name('noti.read');
+        Route::post('/notification-send',function (){$subscribers = Post::query()->find(7);
+//    return $subscribers;
+            foreach ($subscribers as $subscriber) {
+                Log::debug($subscriber);
+            }
+        });
+    });
 
-Route::delete('/sanctum/logout/all', [AuthController::class,'logoutAll'])->middleware(['auth:sanctum']);
+    Route::group([
+        'prefix' => 'setting'
+    ],function (){
+//    Route::get('/subscribed',[SubscribedController::class,'subscribed'])->name('user.subscribed');
+//    Route::get('/unsubscribed',[SubscribedController::class,'unsubscribed'])->name('user.unsubscribed');
+        Route::post('/option',[SubscribedController::class,'option'])->name('user.option');
+    });
+});
 
-Route::post('/sanctum/tokens', [AuthController::class,'tokens'])->middleware(['auth:sanctum']);
+Route::group([
+    'middleware' => ['auth:sanctum','band'],
+    'prefix' => 'administrators'
+],function (){
+    Route::get('/freeze-user',[UserController::class,'freeze'])->name('user.band');
+    Route::get('/unfreeze-user',[UserController::class,'unfreeze'])->name('user.unband');
+    Route::post('/user-access',[UserController::class,'accessUser'])->name('user.access');
+
+    Route::post('/role/toAdmin',[UserController::class,'toAdmin'])->name('user.admin');
+    Route::post('/role/toEditor',[UserController::class,'toEditor'])->name('user.editor');
+    Route::post('/role/toAuthor',[UserController::class,'toAuthor'])->name('user.author');
+    Route::post('/role/toUser',[UserController::class,'toUser'])->name('user.user');
+
+    Route::get('trash',[PostController::class,'trashes'])->name('post.trash');
+    Route::delete('post/forceDelete/{post}',[PostController::class,'forceDelete'])->name('post.force');
+    Route::delete('post/restore/{post}',[PostController::class,'restore'])->name('post.restore');
+
+});

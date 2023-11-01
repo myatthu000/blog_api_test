8aa465246e990f9661431c06296b5b3e232eb6e1
myatthu000 <september.2019thu@gmail.com>
2023-11-01 15:42:34 +0630

    middleware update 3

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Admin;
 use App\Http\Middleware\isBand;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -64,6 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'isBand' => isBand::class,
+        'admin' => Admin::class,
+        'band' => isBand::class,
     ];
 }
